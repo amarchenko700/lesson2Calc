02fa427e2a99fb8537f152276981201c5d876614
amarchenko700 <lexx700@yandex.ru>
2021-06-03 01:00:43 +0300

    Проработаные все функции для портретной ориентации

---
+++ app/src/main/java/com/example/calculator/Calculator.java
@@ -0,0 +1,177 @@
+package com.example.calculator;
+
+import android.view.View;
+import android.widget.TextView;
+
+import MathOperations.*;
+
+public class Calculator {
+
+    private String expressionString;
+    private TextView textView, textViewMemory;
+    private MathGB mgb;
+    private Float memoryValue = 0f;
+
+    public Calculator(TextView tv, TextView tvm) {
+        this.textView = tv;
+        this.textViewMemory = tvm;
+        mgb = new MathGB();
+        expressionString = "";
+    }
+
+    public void inputExpression(View v) {
+
+        switch (v.getId()) {
+            //region numbers_Handlers
+            case R.id.key_0: {
+                expressionString = expressionString + "0";
+                break;
+            }
+            case R.id.key_1: {
+                expressionString = expressionString + "1";
+                break;
+            }
+            case R.id.key_2: {
+                expressionString = expressionString + "2";
+                break;
+            }
+            case R.id.key_3: {
+                expressionString = expressionString + "3";
+                break;
+            }
+            case R.id.key_4: {
+                expressionString = expressionString + "4";
+                break;
+            }
+            case R.id.key_5: {
+                expressionString = expressionString + "5";
+                break;
+            }
+            case R.id.key_6: {
+                expressionString = expressionString + "6";
+                break;
+            }
+            case R.id.key_7: {
+                expressionString = expressionString + "7";
+                break;
+            }
+            case R.id.key_8: {
+                expressionString = expressionString + "8";
+                break;
+            }
+            case R.id.key_9: {
+                expressionString = expressionString + "9";
+                break;
+            }
+            //endregion
+            //region functions
+            case R.id.key_addition: {
+                addOperations("+");
+                break;
+            }
+            case R.id.key_subtraction: {
+                addOperations("-");
+                break;
+            }
+            case R.id.key_multiplication: {
+                addOperations("*");
+                break;
+            }
+            case R.id.key_division: {
+                addOperations("/");
+                break;
+            }
+            //endregion
+            //region memory
+            case R.id.key_MC: {
+                textViewMemory.setVisibility(View.INVISIBLE);
+                memoryValue = 0f;
+                break;
+            }
+            case R.id.key_MR: {
+                expressionString = Float.toString(memoryValue);
+                break;
+            }
+            case R.id.key_MAsc: {
+                putInMemory(getLastNumberFloat(), 1);
+                break;
+            }
+            case R.id.key_MDesc: {
+                putInMemory(getLastNumberFloat(), -1);
+                break;
+            }
+            //endregion
+            case R.id.key_C: {
+                expressionString = "";
+                break;
+
+            }
+            case R.id.key_CE: {
+                char[] expCharArr = expressionString.toCharArray();
+                int indexOperation;
+                for (indexOperation = expCharArr.length - 1; indexOperation >= 0; indexOperation--) {
+                    char currentChar = expCharArr[indexOperation];
+                    if (!mgb.charIsNumber(currentChar)) {
+                        expressionString = String.copyValueOf(expCharArr, 0, indexOperation + 1);
+                        break;
+                    }
+                }
+                break;
+            }
+            case R.id.key_delete: {
+                char[] expCharArr = expressionString.toCharArray();
+                if (expCharArr.length > 0)
+                    expressionString = String.copyValueOf(expCharArr, 0, expCharArr.length - 1);
+                break;
+            }
+            case R.id.key_equally: {
+                mgb.setExpression(expressionString);
+                Float result = mgb.evaluate();
+                expressionString = Float.toString(result);
+                break;
+            }
+            default: {
+                break;
+            }
+
+        }
+
+        textView.setText(expressionString);
+    }
+
+    private void addOperations(String operations) {
+        char[] expCharArr = expressionString.toCharArray();
+        char lastChar = expCharArr[expCharArr.length - 1];
+        if (mgb.charIsNumber(lastChar) || mgb.charIsBracket(lastChar))
+            expressionString = expressionString + operations;
+    }
+
+    private String getLastNumberString(){
+        String lastNumber = "";
+        char[] expCharArr = expressionString.toCharArray();
+        int indexOperation;
+        for (indexOperation = expCharArr.length - 1; indexOperation >= 0; indexOperation--) {
+            char currentChar = expCharArr[indexOperation];
+            if (mgb.charIsNumber(currentChar)) {
+                lastNumber = currentChar + lastNumber;
+            }else {
+                break;
+            }
+        }
+        return lastNumber;
+    }
+
+    private Float getLastNumberFloat(){
+        Float lastNumber = 0f;
+        String lastNumberString = getLastNumberString();
+        lastNumber = Float.parseFloat(lastNumberString);
+        return lastNumber;
+    }
+
+    private void putInMemory(Float value, int koef){
+        memoryValue = memoryValue + value * koef;
+        textViewMemory.setVisibility(View.VISIBLE);
+    }
+
+}
+
--- app/src/main/java/com/example/calculator/MainActivity.java
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -1,9 +1,9 @@
 package com.example.calculator;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
-import android.widget.Button;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,32 +12,54 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button keyMC = findViewById(R.id.MC);
-        Button keyMR = findViewById(R.id.MR);
-        Button keyMAsc = findViewById(R.id.MAsc);
-        Button keyMDesc = findViewById(R.id.MDesc);
-        Button keyMS = findViewById(R.id.MS);
+        TextView textView = findViewById(R.id.expressionString);
+        TextView textViewMemory = findViewById(R.id.textMemory);
+        textViewMemory.setVisibility(View.INVISIBLE);
+        Calculator calculator = new Calculator(textView, textViewMemory);
 
-        Button key_percent = findViewById(R.id.key_percent);
-        Button key_CE = findViewById(R.id.key_CE);
-        Button key_C = findViewById(R.id.key_C);
-        Button key_delete = findViewById(R.id.key_delete);
+        CalcButtonListener calcButtonListener = new CalcButtonListener(calculator);
 
-        Button key0 = findViewById(R.id.key_0);
-        Button key1 = findViewById(R.id.key_1);
-        Button key2 = findViewById(R.id.key_2);
-        Button key3 = findViewById(R.id.key_3);
-        Button key4 = findViewById(R.id.key_4);
-        Button key5 = findViewById(R.id.key_5);
-        Button key6 = findViewById(R.id.key_6);
-        Button key7 = findViewById(R.id.key_7);
-        Button key8 = findViewById(R.id.key_8);
-        Button key9 = findViewById(R.id.key_9);
+        findViewById(R.id.key_MC).setOnClickListener(calcButtonListener);
+        findViewById(R.id.key_MR).setOnClickListener(calcButtonListener);
+        findViewById(R.id.key_MAsc).setOnClickListener(calcButtonListener);
+        findViewById(R.id.key_MDesc).setOnClickListener(calcButtonListener);
 
-        Button key_subtraction = findViewById(R.id.key_subtraction);
-        Button key_addition = findViewById(R.id.key_addition);
-        Button key_inverting = findViewById(R.id.key_inverting);
-        Button key_equally = findViewById(R.id.key_equally);
+        findViewById(R.id.key_CE).setOnClickListener(calcButtonListener);
+        findViewById(R.id.key_C).setOnClickListener(calcButtonListener);
+        findViewById(R.id.key_delete).setOnClickListener(calcButtonListener);
+
+        findViewById(R.id.key_0).setOnClickListener(calcButtonListener);
+        findViewById(R.id.key_1).setOnClickListener(calcButtonListener);
+        findViewById(R.id.key_2).setOnClickListener(calcButtonListener);
+        findViewById(R.id.key_3).setOnClickListener(calcButtonListener);
+        findViewById(R.id.key_4).setOnClickListener(calcButtonListener);
+        findViewById(R.id.key_5).setOnClickListener(calcButtonListener);
+        findViewById(R.id.key_6).setOnClickListener(calcButtonListener);
+        findViewById(R.id.key_7).setOnClickListener(calcButtonListener);
+        findViewById(R.id.key_8).setOnClickListener(calcButtonListener);
+        findViewById(R.id.key_9).setOnClickListener(calcButtonListener);
+
+        findViewById(R.id.key_subtraction).setOnClickListener(calcButtonListener);
+        findViewById(R.id.key_addition).setOnClickListener(calcButtonListener);
+        findViewById(R.id.key_division).setOnClickListener(calcButtonListener);
+        findViewById(R.id.key_multiplication).setOnClickListener(calcButtonListener);
+
+        findViewById(R.id.key_equally).setOnClickListener(calcButtonListener);
 
     }
+
+    class CalcButtonListener implements View.OnClickListener {
+
+        private Calculator calculator;
+
+        public CalcButtonListener(Calculator calculator) {
+            this.calculator = calculator;
+        }
+
+        @Override
+        public void onClick(View v) {
+            calculator.inputExpression(v);
+        }
+    }
+
 }
